b1435d5206c604668f0b0b4de4da1ac274b877f1
SergioMelado <sergio.melado@soprasteria.com>
2024-05-28 15:32:20 +0200

    http client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,17 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([{ path: '', component: ProductListComponent },
     { path: 'products/:productId', component: ProductDetailsComponent },
-    { path: 'cart', component: CartComponent },]),
+    { path: 'cart', component: CartComponent },
+    { path: 'shipping', component: ShippingComponent},]),
   ],
   declarations: [
     AppComponent,
@@ -26,6 +29,7 @@ import { CartComponent } from './cart/cart.component';
     ProductAlertsComponent,
     ProductDetailsComponent,
     CartComponent,
+    ShippingComponent,
   ],
   bootstrap: [AppComponent],
 })
